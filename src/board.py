d85ca02f73dacb28a1cb3e36bd40f4fb166d4224
Mahesh Shivamallappa <mahesh.shivamallappa@c02w239phv2q.gateway>
2020-05-15 20:54:01 +1000

    Changed PLACEHOLDER from a global variable to a constant in Board class

--- src/board.py
+++ src/board.py
@@ -1,6 +1,5 @@
-PLACEHOLDER = " "
 class Board():
-
+    PLACEHOLDER = " "
     def __init__(self):
         self.board = self.__create_empty_board()
         #Keep winning combination a private attribute
@@ -11,7 +10,7 @@ class Board():
     def __create_empty_board(self):
         board = ["#"]
         for i in range(1,10): #Create an empty board with 9 blank elements
-            board.append(PLACEHOLDER)
+            board.append(self.PLACEHOLDER)
         return board
 
     #Display the board
@@ -23,11 +22,11 @@ class Board():
 
     #Given a player's choice of position check if it is available for use
     def space_check(self, position):
-        return self.board[position] == PLACEHOLDER
+        return self.board[position] == self.PLACEHOLDER
 
     #Check if the board is full
     def full_board_check(self):
-        return len([i for i in self.board if i == PLACEHOLDER]) == 0
+        return len([i for i in self.board if i == self.PLACEHOLDER]) == 0
 
     #Check if a given marker has won based on __winning_combination
     def win_check(self, marker):
